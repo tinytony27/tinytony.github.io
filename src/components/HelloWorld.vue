b8e80beae7e76f0d47b5ca4219c4c417e2fef700
m-rac <33270318+m-rac@users.noreply.github.com>
2022-10-11 20:48:57 +0900

    TypeScriptに変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,12 +32,16 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
   props: {
     msg: String
   }
+})
+export default class HelloWorld extends Vue {
+  msg!: string
 }
 </script>
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    future: {},
+    content: [
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
